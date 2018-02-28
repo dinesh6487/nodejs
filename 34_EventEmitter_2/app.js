7015c029529b483ad1581218310bdf6ab1557239
Dinesh Elumalai <dineshkumar.edk@gmail.com>
2018-02-28 15:02:13 -0800

    Node Event Config

--- 34_EventEmitter_2/app.js
+++ 34_EventEmitter_2/app.js
@@ -1,14 +1,17 @@
 var Emitter = require('events');
 
+//Creating a config file will help in maintaining the events
+var eventConfig = require('./config').events;
+
 var emter = new Emitter();
 
-emter.on('greet',function(){
+emter.on(eventConfig.GREET,function(){
     console.log('Event emitter on for greet 1')
 });
 
-emter.on('greet',function(){
+emter.on(eventConfig.GREET,function(){
     console.log('Event emitter on for greet 2')
 });
 
 
-emter.emit('greet');
+emter.emit(eventConfig.GREET);
