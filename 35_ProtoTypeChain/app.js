cd286f9e4dd9d8eddf4d002a80be3ef4d7b032ad
dinesh6487 <dineshkumar.edk@gmail.com>
2018-03-05 23:50:26 -0800

    playing with object.create

--- 35_ProtoTypeChain/app.js
+++ 35_ProtoTypeChain/app.js
@@ -1,24 +1,27 @@
 var person = {
     firstName: '',
     lastName:'',
+    sirName:'Mr.',
     greet: function(){
         console.log(this.firstName+' '+this.lastName);
     }
 }
 
 var dinesh = Object.create(person);
+console.log(person);
 dinesh.firstName = 'Dinesh';
 dinesh.lastName = 'Elumalai';
 dinesh.middleName= 'Kumar';
 dinesh.greet();
 dinesh.__proto__.firstName = 'Test';
 console.log(dinesh);
+console.log(person);
 
 var karthik = Object.create(dinesh);
-
+console.log(person);
 karthik.__proto__.__proto__.lastName = 'Temp';
 
 var aone = Object.create(person);
-
+console.log(person);
 
 console.log('');
