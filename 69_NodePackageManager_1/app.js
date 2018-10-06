84c96f7fba2553606a0e69e35e8a4df16b71ad5a
dinesh6487 <dineshkumar.edk@gmail.com>
2018-10-06 01:30:52 -0700

    Node Package Manager

---
+++ 69_NodePackageManager_1/app.js
@@ -0,0 +1,5 @@
+// To create package.json run npm init. Enter required values. This will create and initilize package.json
+// To install package use npm install moment --save, This add an entry to package.json 
+
+var moment = require('moment');
+console.log(moment().format());
