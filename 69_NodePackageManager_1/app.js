cb6ac88a5bdbce11ecdc86e33df27b3c822c989e
dinesh6487 <dineshkumar.edk@gmail.com>
2018-10-06 01:39:54 -0700

    npm

--- 69_NodePackageManager_1/app.js
+++ 69_NodePackageManager_1/app.js
@@ -1,5 +1,6 @@
 // To create package.json run npm init. Enter required values. This will create and initilize package.json
 // To install package use npm install moment --save, This add an entry to package.json 
-
+// For dev depenencies use npm install jasmine-node --save-dev
+// For global install npm install -g nodemon
 var moment = require('moment');
 console.log(moment().format());
