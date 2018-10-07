5fd7b62bc92a78a63e43e0d28c9e70d67e1dbe7d
dinesh6487 <dineshkumar.edk@gmail.com>
2018-10-07 02:01:45 -0700

    Express Api;s

--- 73_Express/app.js
+++ 73_Express/app.js
@@ -1,15 +1,24 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 3000;
+// Best pratice to use enviroment variables
+var port = process.env.PORT || 3002;
 
+// Sending html in response, express will take care of adding contect type to the headers
 app.get('/',function(req,res){
 
     res.send('<html><title>Node Html</title><head></head><body><h1>Hello World!</h1></body></html>');
 });
 
+// Sending JSON to the response using json function and passing object
 app.get('/api',function(req,res){
     res.json({fName:'Dinesh',lName:'Elumalai'})
+});
+
+// API for using request parameter
+app.get('/Person/:id',function(req,res){
+
+    res.send('<html><title>Node Html</title><head></head><body><h1>Hello Person '+ req.params.id +'</h1></body></html>');
 });
 
 app.listen(port);
